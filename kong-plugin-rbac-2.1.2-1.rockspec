b477cd718a2b7a84d4848354bd1df9edddac0ac0
Mike <huanghaiying@worldopennetwork.net>
2019-03-06 18:21:48 +0800

    update schema

--- kong-plugin-rbac-2.1.2-1.rockspec
+++ kong-plugin-rbac-2.1.2-1.rockspec
@@ -6,7 +6,7 @@ supported_platforms = {"linux", "macosx"}
 source = {
   -- these are initially not required to make it work
   url = "git://github.com/hhy5861/kong-rbac",
-  tag = "2.1.1"
+  tag = "2.1.2"
 }
 version = source.tag .. "-1"
 
--- kong/plugins/rbac/daos.lua
+++ kong/plugins/rbac/daos.lua
@@ -1,65 +1,67 @@
 local utils = require "kong.tools.utils"
 
 local RESOURCE_SCHEMA = {
-  primary_key = { "id" },
+  primary_key = {"id"},
   table = "rbac_resources",
-  cache_key = { "api_id" },
+  cache_key = {"api_id"},
   fields = {
-    id = { type = "id", dao_insert_value = true },
-    api_id = { type = "uuid", required = true, foreign = "apis:id" },
-    method = { type = "string", required = true },
-    upstream_path = { type = "string", required = true },
-    description = { type = "string", required = true },
-    visibility = { type = "string", required = false, default = "protected" },
-    created_at = { type = "timestamp", dao_insert_value = true }
-  },
+    id = {type = "id", dao_insert_value = true},
+    api_id = {type = "uuid", required = true, foreign = "apis:id"},
+    method = {type = "string", required = true},
+    upstream_path = {type = "string", required = true},
+    description = {type = "string", required = true},
+    visibility = {type = "string", required = false, default = "protected"},
+    created_at = {type = "timestamp", dao_insert_value = true}
+  }
 }
 
 local ROLE_SCHEMA = {
-  primary_key = { "id" },
+  primary_key = {"id"},
   table = "rbac_roles",
-  cache_key = { "id" },
+  cache_key = {"id"},
   fields = {
-    id = { type = "id", dao_insert_value = true },
-    name = { type = "string", required = true, unique = true },
-    description = { type = "string", required = false },
-    created_at = { type = "timestamp", dao_insert_value = true }
+    id = {type = "id", dao_insert_value = true},
+    consumer_id = {type = "string", required = true, foreign = "consumers:id"},
+    name = {type = "string", required = true, unique = true},
+    disabled = {type = "boolean", required = true},
+    description = {type = "string", required = false},
+    created_at = {type = "timestamp", dao_insert_value = true}
   }
 }
 
 local ROLE_RESOURCE_SCHEMA = {
-  primary_key = { "id" },
+  primary_key = {"id"},
   table = "rbac_role_resources",
-  cache_key = { "role_id" },
+  cache_key = {"role_id"},
   fields = {
-    id = { type = "id", dao_insert_value = true },
-    role_id = { type = "string", required = true, foreign = "rbac_roles:id" },
-    resource_id = { type = "string", required = true, foreign = "rbac_resources:id" }
+    id = {type = "id", dao_insert_value = true},
+    role_id = {type = "string", required = true, foreign = "rbac_roles:id"},
+    resource_id = {type = "string", required = true, foreign = "rbac_resources:id"}
   }
 }
 
 local ROLE_CONSUMER_SCHEMA = {
-  primary_key = { "id" },
+  primary_key = {"id"},
   table = "rbac_role_consumers",
-  cache_key = { "consumer_id" },
+  cache_key = {"consumer_id"},
   fields = {
-    id = { type = "id", dao_insert_value = true },
-    role_id = { type = "string", required = true, foreign = "rbac_roles:id" },
-    consumer_id = { type = "string", required = true, foreign = "consumers:id" }
+    id = {type = "id", dao_insert_value = true},
+    role_id = {type = "string", required = true, foreign = "rbac_roles:id"},
+    consumer_id = {type = "string", required = true, foreign = "consumers:id"}
   }
 }
 
 local CREDENTIAL_SCHEMA = {
-  primary_key = { "id" },
+  primary_key = {"id"},
   table = "rbac_credentials",
-  cache_key = { "key" },
+  cache_key = {"key"},
   fields = {
-    id = { type = "id", dao_insert_value = true },
-    created_at = { type = "timestamp", immutable = true, dao_insert_value = true },
-    expired_at = { type = "timestamp" },
-    consumer_id = { type = "id", required = true, foreign = "consumers:id" },
-    key = { type = "string", required = false, unique = true, default = utils.random_string }
-  },
+    id = {type = "id", dao_insert_value = true},
+    created_at = {type = "timestamp", immutable = true, dao_insert_value = true},
+    expired_at = {type = "timestamp"},
+    consumer_id = {type = "id", required = true, foreign = "consumers:id"},
+    key = {type = "string", required = false, unique = true, default = utils.random_string}
+  }
 }
 
 return {
